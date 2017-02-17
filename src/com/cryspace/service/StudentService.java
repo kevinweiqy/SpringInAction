64049d3d8d25b82d532720cf898439a55f3197a8
kevin <kevinweiqy@gmail.com>
2017-02-17 20:25:37 +0800

    add several files

---
+++ src/com/cryspace/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.cryspace.service;
+
+/**
+ * Created by bill on 2017/2/17.
+ */
+public interface StudentService {
+}
---
+++ src/com/cryspace/service/serviceImpl/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cryspace.service.serviceImpl;
+
+import com.cryspace.service.StudentService;
+
+/**
+ * Created by bill on 2017/2/17.
+ */
+public class StudentServiceImpl implements StudentService {
+}
