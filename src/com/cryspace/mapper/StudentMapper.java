6833db06f765e7ca4f841f648bbc8d816112f677
kevin <kevinweiqy@gmail.com>
2017-02-18 22:50:41 +0800

    add a new test method

--- src/com/cryspace/mapper/StudentMapper.java
+++ src/com/cryspace/mapper/StudentMapper.java
@@ -3,6 +3,8 @@ package com.cryspace.mapper;
 import com.cryspace.model.Student;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by bill on 2017/2/18.
  */
@@ -13,6 +15,11 @@ public interface StudentMapper {
     public int addStudent(Student student);
 
     public int updateStudent(Student student);
+
+    public int deleteStudent(String id);
+
+    public List<Student> getStudentList();
+
 
 
 }
